1f4f44531e84aa993c2432ce98c2ce4d506e0871
Pramita Shelke <pramita.shelke28@gmail.com>
2024-11-13 23:08:49 +0530

    yes

--- Fintech/Fintech/settings.py
+++ Fintech/Fintech/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Fintech.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
